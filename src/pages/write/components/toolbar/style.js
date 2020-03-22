06cbc4acede0161637c946ef5e5c7b0f280e4d3e
JiaoJinchao <314279802@qq.com>
2020-03-22 19:34:04 +0800

    写文章界面增加css样式

--- src/pages/write/components/toolbar/style.js
+++ src/pages/write/components/toolbar/style.js
@@ -10,10 +10,56 @@ export const EditorWrapper =styled.div`
 `
 
 export const TitleWrapper = styled.div`
+    font-size: 15px;
+    font-weight:bold;
 `
 
 export const DomainItemWrapper =styled.div`
+    display: inline-block;
+    zoom: 1;
+    *: ;
+    display: inline;
+    vertical-align: baseline;
+    margin: 0 1px;
+    outline: none;
+    cursor: pointer;
+    text-align: center;
+    text-decoration: none;
+    font: 14px/100% Arial, Helvetica, sans-serif;
+    padding: .5em 2em .20em;
+    text-shadow: 0 1px 1px rgba(0,0,0,.3);
+    -webkit-border-radius: .500em;
+    -moz-border-radius: .5em;
+    border-radius: .5em;
+    -webkit-box-shadow: 0 1px 2px rgba(0,0,0,.2);
+    -moz-box-shadow: 0 1px 2px rgba(0,0,0,.2);
+    box-shadow: 0 1px 2px rgba(0,0,0,.2);
+    font-size: 11px;
+    color: #606060;
+    border: solid 1px #b7b7b7;
 `
 
 export const TopicItemWrapper =styled.div`
+display: inline-block;
+    zoom: 1;
+    *: ;
+    display: inline;
+    vertical-align: baseline;
+    margin: 0 1px;
+    outline: none;
+    cursor: pointer;
+    text-align: center;
+    text-decoration: none;
+    font: 14px/100% Arial, Helvetica, sans-serif;
+    padding: .5em 2em .10em;
+    text-shadow: 0 1px 1px rgba(0,0,0,.3);
+    -webkit-border-radius: .20em;
+    -moz-border-radius: .5em;
+    border-radius: .5em;
+    -webkit-box-shadow: 0 1px 2px rgba(0,0,0,.2);
+    -moz-box-shadow: 0 1px 2px rgba(0,0,0,.2);
+    box-shadow: 0 1px 2px rgba(0,0,0,.2);
+    font-size: 11px;
+    color: #606060;
+    border: solid 1px #b7b7b7;
 `
