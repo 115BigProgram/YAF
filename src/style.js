e61310f2d9295f8ccc5eeefc13ba7691e910363d
115BigProgram <2447564455@qq.com>
2020-03-24 12:55:41 +0800

    bug: build result dont have markdown style

--- src/style.js
+++ src/style.js
@@ -1,16 +1,15 @@
 import styled, { injectGlobal } from "styled-components";
-import {ContentHorizontalPadding, ContentLength, TopNavBarHeight,DesktopMiniWidth} from "./constvars";
+import { ContentHorizontalPadding, ContentLength, TopNavBarHeight, DesktopMiniWidth } from "./constvars";
 
 injectGlobal`
 	html, body, div, span, applet, object, iframe,
-	h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+	blockquote, pre,
 	a, abbr, acronym, address, big, cite, code,
 	del, dfn, em, img, ins, kbd, q, s, samp,
 	small, strike, strong, sub, sup, tt, var,
 	b, u, i, center,
-	dl, dt, dd, ol, ul, li,
+	dl, dt, dd, ol, 
 	fieldset, form, label, legend,
-	table, caption, tbody, tfoot, thead, tr, th, td,
 	article, aside, canvas, details, embed, 
 	figure, figcaption, footer, header, hgroup, 
 	menu, nav, output, ruby, section, summary,
@@ -52,7 +51,7 @@ export const MainContainer = styled.div`
   padding-top:15px;
 `
 
-export const ContentContainer =styled.div`
+export const ContentContainer = styled.div`
   @media (min-width: ${DesktopMiniWidth}px){
     margin-left:${ContentHorizontalPadding};
     width:${ContentLength};
